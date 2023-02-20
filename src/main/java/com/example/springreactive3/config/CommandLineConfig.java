88dcc0b94d6059f7ee2e75882d60f81b6deed733
Anatolii <anatolii.melchenko@academysmart.com>
2023-02-20 21:51:21 +0200

    sout;

--- src/main/java/com/example/springreactive3/config/CommandLineConfig.java
+++ src/main/java/com/example/springreactive3/config/CommandLineConfig.java
@@ -5,12 +5,14 @@ import com.example.springreactive3.repo.LanguageRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Configuration
 public class CommandLineConfig {
 
     @Bean
     public CommandLineRunner preload(LanguageRepository languageRepository) {
+        System.out.println("Generating test values for collection : " + Language.class.getAnnotation(Document.class).collection());
         return args -> {
             languageRepository.save(new Language("Java", "Gosling", "imperative"));
             languageRepository.save(new Language("Scala", "Odersly", "functional"));
